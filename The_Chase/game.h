4863af205a336d52b1e22e972ab12d75a50eef7c
Mausham Neupane <maushamneupane2060@gmail.com>
2024-02-04 00:06:56 +0545

    added collide and dead sounds

--- The_Chase/game.h
+++ The_Chase/game.h
@@ -31,11 +31,9 @@ private:
 	int p_e_collision_count = 0;
 	int big_e_spawn_interval = 0;
 
-
 	float delta_time = 0;
 	float e_spawn_interval = 3.0f;
 	
-
 	void move_entity();
 	void handle_input();
 	void render();
@@ -51,11 +49,13 @@ private:
 	bool is_game_paused = false;
 	bool is_game_exit = false;
 
-	std::shared_ptr<Sound> background_sound_1 = std::make_shared<Sound>("background_1", true, 20);
-	std::shared_ptr<Sound> background_sound_2 = std::make_shared<Sound>("background_2", true, 30);
-	std::shared_ptr<Sound> bullet_sound = std::make_shared<Sound>("bullet", false, 60);
-	std::shared_ptr<Sound> big_e_spawn_sound = std::make_shared<Sound>("big_e_spawn", false, 60);
-	std::shared_ptr<Sound> collision_sound = std::make_shared<Sound>("collision", false, 10);
+	std::shared_ptr<Sound> background_sound_1 = std::make_shared<Sound>("background_1", true, 25);
+	std::shared_ptr<Sound> background_sound_2 = std::make_shared<Sound>("background_2", true, 35);
+	std::shared_ptr<Sound> bullet_sound = std::make_shared<Sound>("bullet", false, 50);
+	std::shared_ptr<Sound> big_e_spawn_sound = std::make_shared<Sound>("big_e_spawn", false, 15);
+	std::shared_ptr<Sound> big_e_dead_sound = std::make_shared<Sound>("big_e_dead", false, 35);
+	std::shared_ptr<Sound> collision_sound = std::make_shared<Sound>("collision", false, 20);
+	std::shared_ptr<Sound> player_dead_sound = std::make_shared<Sound>("player_dead", false, 50);
 
 	std::shared_ptr<Show_text> score_text = std::make_shared<Show_text>(sf::Vector2f(30, 10));
 	std::shared_ptr<Show_text> rem_life_text = std::make_shared<Show_text>(sf::Vector2f(30, 85));
