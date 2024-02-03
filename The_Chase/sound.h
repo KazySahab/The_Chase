59f8abb07c7bfdc0705d4da2f243e8c1773418b0
Mausham Neupane <maushamneupane2060@gmail.com>
2024-02-04 01:05:20 +0545

    big_e random spawn sound added

--- The_Chase/sound.h
+++ The_Chase/sound.h
@@ -9,6 +9,11 @@ public:
 	sf::Music sound;
 	bool is_paused = false;
 	Sound(){}
+
+	Sound(bool to_repeat,const float vol){
+		sound.setLoop(to_repeat);
+		sound.setVolume(vol);
+	}
 	Sound(const std::string name, bool to_repeat,const float vol )
 	{
 		if (!sound.openFromFile("music/" + name + ".mp3"))
@@ -21,6 +26,13 @@ public:
 			sound.setVolume(vol);
 		}
 		
+	}
+	void set_choices(const std::string name)
+	{
+		if (!sound.openFromFile("music/" + name + ".mp3"))
+		{
+			std::cout << "cant open audio file" << name << std::endl;
+		}
 	}
 };
 
